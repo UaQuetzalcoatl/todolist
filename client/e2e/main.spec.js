67da618d42bfacf85faaf1e6793806860da479fe
Alex <ua.quetzalcoatl@gmail.com>
2015-09-10 15:56:11 +0300

    added simple e2e test

--- client/e2e/main.spec.js
+++ client/e2e/main.spec.js
@@ -1,21 +1,25 @@
 'use strict';
 
 describe('The main view', function () {
-  var page;
+  var page, apiUrl;
 
   beforeEach(function () {
-    browser.get('/index.html');
+    browser.get('/');
     page = require('./main.po');
   });
 
-  it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
-    expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
-    expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+  it('should include buttons', function() {
+    expect(page.homeButton.getText()).toBe('HOME');
+    expect(page.pointButton.getText()).toBe('POINTS');
   });
 
-  it('should list more than 5 awesome things', function () {
-    expect(page.thumbnailEls.count()).toBeGreaterThan(5);
-  });
+  it('should have correct routes', function () {
+    page.pointButton.click().then(function () {
+      expect(browser.getCurrentUrl()).toContain('/points');
+    });
 
+    page.homeButton.click().then(function () {
+      expect(browser.getCurrentUrl()).not.toContain('/points');
+    });
+  });
 });
